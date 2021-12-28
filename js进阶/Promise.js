141fd2db210d11ab83b354b2dc0aee53164686ba
songyipeng <songyipeng@cmhi.chinamobile.com>
2021-12-28 19:20:23 +0800

    feat:add

--- js进阶/Promise.js
+++ js进阶/Promise.js
@@ -15,17 +15,31 @@ function myPromise(excutor) {
     this.rejectFuncs = [];
 
     const resolve = value => {
-        if (this.state === 'pending') {
-            this.state = 'fulfilled';
-            this.value = value;
-        }
+        // 为了支持同步方法，需要延后，否则会因为方法还没传过来就调用而遗漏调用
+        // 利用setTimeout特性将具体执行放到then之后
+        setTimeout(() => {
+            if (this.state === 'pending') {
+                this.state = 'fulfilled';
+                this.value = value;
+                this.resolveFuncs.forEach(func => {
+                    func(value);
+                })
+            }
+        })
     }
 
     const reject = reason => {
-        if (this.state === 'pending') {
-            this.state = 'rejected';
-            this.reason = reason;
-        }
+        // 为了支持同步方法，需要延后，否则会因为方法还没传过来就调用而遗漏调用
+        // 利用setTimeout特性将具体执行放到then之后
+        setTimeout(() => {
+            if (this.state === 'pending') {
+                this.state = 'rejected';
+                this.reason = reason;
+                this.rejectFuncs.forEach(func => {
+                    func(reason);
+                })
+            }
+        })
     }
 
     // 执行执行器函数
@@ -38,11 +52,83 @@ function myPromise(excutor) {
 }
 
 // then 方法是promise的原型方法
-// 接受一个成功回调f1和一个失败回调f2
-// f1接收成功的返回值，f2接受失败的返回值
-myPromise.prototype.then = function(f1, f2) {
-
+// 接受一个成功回调onFulfilled和一个失败回调onRejected
+// onFulfilled接收成功的返回值，onRejected接受失败的返回值
+MyPromise.prototype.then = function(onFulfilled, onRejected) {
+    const self = this;
+    let bridgePromise;
+    //防止使用者不传成功或失败回调函数，所以成功失败回调都给了默认回调函数
+    onFulfilled = typeof onFulfilled === "function" ? onFulfilled : value => value;
+    onRejected = typeof onRejected === "function" ? onRejected : error => { throw error };
+    if (self.state === 'fulfilled') {
+        return bridgePromise = new MyPromise((resolve, reject) => {
+            setTimeout(() => {
+                try {
+                    let x = onFulfilled(self.value);
+                    resolvePromise(bridgePromise, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        })
+    }
+    if (self.state === 'rejected') {
+        return bridgePromise = new MyPromise((resolve, reject) => {
+            setTimeout(() => {
+                try {
+                    let x = onRejected(self.error);
+                    resolvePromise(bridgePromise, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        });
+    }
+    if (self.state === 'pending') {
+        return bridgePromise = new MyPromise((resolve, reject) => {
+            self.resolveFuncs.push((value) => {
+                try {
+                    let x = onFulfilled(value);
+                    resolvePromise(bridgePromise, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+            self.rejectFuncs.push((error) => {
+                try {
+                    let x = onRejected(error);
+                    resolvePromise(bridgePromise, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        });
+    }
 }
+//catch方法其实是个语法糖，就是只传onRejected不传onFulfilled的then方法
+MyPromise.prototype.catch = function(onRejected) {
+    return this.then(null, onRejected);
+}
+
+//用来解析回调函数的返回值x，x可能是普通值也可能是个promise对象
+function resolvePromise(bridgePromise, x, resolve, reject) {
+    //如果x是一个promise
+     if (x instanceof MyPromise) {
+         //如果这个promise是pending状态，就在它的then方法里继续执行resolvePromise解析它的结果，直到返回值不是一个pending状态的promise为止
+         if (x.status === PENDING) {
+             x.then(y => {
+                 resolvePromise(bridgePromise, y, resolve, reject);
+             }, error => {
+                 reject(error);
+             });
+         } else {
+             x.then(resolve, reject);
+         }
+         //如果x是一个普通值，就让bridgePromise的状态fulfilled，并把这个值传递下去
+     } else {
+         resolve(x);
+     }
+ }
 
 Promise.prototype.all = function(arr) {
     return new Promise((resolve, reject) => {
